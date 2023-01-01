7da2e802adeeb5095a3ad3a3140ca2abc1ef70bd
Mr. MacKenty <bmackenty@gmail.com>
2023-01-01 20:45:06 +0100

    initial commit of proc_gen_npc.py

---
+++ proc_gen_npc.py
@@ -0,0 +1,15 @@
+# procedural generation of npc
+
+# LIVE: Where does the {} live?,
+# KNOWN: What is the {} known for?,
+# NAME: What is {}s name?,
+# LIKE: What does the {} like to do?,
+# DISLIKE: What does the {} not like to do?,
+# FROM: Where is the {} from?,
+# GOING: Where is the {} going?,
+# DESIRE: What does the {} desire?,
+# MOTIVATED: What is {} motivated by?,
+# DOING: What is {} doing right now?,
+# UNIQUE: What is unique about the {}?,
+# DESCRIBE: How is the {} described?
+
